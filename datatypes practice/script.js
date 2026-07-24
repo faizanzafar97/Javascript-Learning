252e5fd72fdfc2cffd2600e222bf4f0225c0646e
faizanzafar97 <faizanzafar8672@gmail.com>
2026-07-24 16:53:13 +0500

    datatypes practice

---
+++ datatypes practice/script.js
@@ -0,0 +1,41 @@
+// string
+
+{let a= 'faizan';
+let b="faizan";}
+
+//number
+
+var a=23;
+var a=23.4;
+
+//boolean true or false
+
+{
+    let a=true;
+    let b= false;
+
+}
+
+//null jan boche kar value na dena 
+
+{let selected_students=null;}
+
+
+//undefined system khud se go value de 
+
+{let a;}
+
+//symbol  unique immutable value na change hona wali
+
+let obj={
+    uid:1,
+    name: "faizan"
+};
+
+let u1 = Symbol("uid");
+obj[uid]="001";
+
+//BigInt format
+
+let a=123456789876543212345678n;
+
