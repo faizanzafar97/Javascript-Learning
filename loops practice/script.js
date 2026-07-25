eaf1c5751ca66ebd40238fcb94bbb5b43776037a
faizanzafar97 <faizanzafar8672@gmail.com>
2026-07-25 16:52:45 +0500

    loops practice

---
+++ loops practice/script.js
@@ -0,0 +1,24 @@
+//loops
+
+//for loop
+
+for(let i=1;i<=10;i++){
+
+    console.log(i);
+}
+
+// print even
+
+for(let i=1;i<21;i++){
+
+    if (i%2===0){
+        console.log(i);
+    }
+    
+}
+//table of 5
+for(let i= 1 ;i<11;i++){
+
+    console.log("5 x ",i," = ",i*5);
+
+}
