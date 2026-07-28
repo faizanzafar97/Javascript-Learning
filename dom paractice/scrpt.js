94f0f4d3f2a782c774103e03c21f7c6d25e4bc07
faizanzafar97 <faizanzafar8672@gmail.com>
2026-07-28 15:53:43 +0500

    some more parctice of dom

--- dom paractice/scrpt.js
+++ dom paractice/scrpt.js
@@ -1,4 +1,34 @@
 //dom advance pactice
 
-let p=document.querySelector("#faizan");
-p.textContent="welcome to sherryains";
+//let p=document.querySelector("#faizan");
+//p.textContent="welcome to sherryains";
+
+
+// practice
+
+let p= document.querySelector("p");
+
+p.innerHTML="<b>updated</b> by java script";
+
+
+// how do you get src of image using js 
+
+let img= document.querySelector("img");
+
+console.log(img.src);
+
+console.dir(img.getAttribute("src"));
+
+//
+
+
+let img1= document.querySelector("img").setAttribute("src","https://wallpapercave.com/fwp-510/wp13907873.jpg");
+
+
+//creat a new list 
+let ul=document.querySelector("ul");
+let li=document.createElement("li");
+
+li.textContent="new task";
+
+ul.appendChild(li);
