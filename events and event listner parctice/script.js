27f9932328e725144870ee987bc7c721c688461e
faizanzafar97 <faizanzafar8672@gmail.com>
2026-07-28 16:41:39 +0500

    event listener practice

---
+++ events and event listner parctice/script.js
@@ -0,0 +1,26 @@
+// Practice of Event Listener
+
+let h1 = document.querySelector("h1");
+
+h1.addEventListener("click", function () {
+    h1.style.color = "red";
+});
+
+let p = document.querySelector("p");
+
+function changeBackground() {
+    p.style.backgroundColor = "black";
+    p.style.color = "white";
+    p.style.padding = "10px";
+}
+
+p.addEventListener("click", changeBackground);
+
+// Input event
+let input = document.querySelector("input");
+
+if (input) {
+    input.addEventListener("input", function (a) {
+        if(a.data!==null) console.log(a.data);
+    });
+}
