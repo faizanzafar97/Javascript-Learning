90b66ef70ed3724eadafe6978106c42957372890
faizanzafar97 <faizanzafar8672@gmail.com>
2026-07-31 16:37:02 +0500

    updted one of car making

--- events and event listner parctice/t.js
+++ events and event listner parctice/t.js
@@ -4,7 +4,23 @@ form.addEventListener("submit",function(e){
 
     e.preventDefault();
 
-    console.log(e.target[0].value,inputs[1].value,inputs[2].value,inputs[3].value);
+     let card_container=document.createElement("div");
+    card_container.classList.add("card-container");
+
+    let card=document.createElement("div");
+    card.classList.add("card"); 
+
+    let img=document.createElement("img");
+    img.setAttribute("src",e.target[0].value)
+    let h2=document.createElement("h2");
+    h3.textContent=e.target[1].value;
+    let h4 =document.createElement("h4");
+    h4.textContent=e.target[2].value;
+    let p=document.createElement("p");
+
+    p.textContent=e.target[3].value;
+
+   
    
 
-})
+});
