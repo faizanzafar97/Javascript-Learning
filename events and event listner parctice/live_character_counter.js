9b5306c0e40a46f85bbfbc9dcb9f9fb7e8ac1ac8
faizanzafar97 <faizanzafar8672@gmail.com>
2026-08-03 15:58:05 +0500

    updated live counter

--- events and event listner parctice/live_character_counter.js
+++ events and event listner parctice/live_character_counter.js
@@ -1,15 +1,20 @@
-let inp= document.querySelector("input");
-let span= document.querySelector("span");
-inp.addEventListener("input", function(){
+let inp = document.querySelector("input");
+let span = document.querySelector("span");
 
-   let left_count= 20 - inp.value.length;
-   span.textContent=left_count;
+inp.addEventListener("input", function () {
 
-   if(left_count<0){
-    span.style.color="red";
-   }
-   else{
-    span.style.color="black";
-   }
+    let left_count = 20 - inp.value.length;
+    span.textContent = left_count;
+
+    if (left_count < 0) {
+        span.style.color = "red";
+        inp.style.borderColor = "red";
+    } else if (left_count <= 5) {
+        span.style.color = "orange";
+        inp.style.borderColor = "orange";
+    } else {
+        span.style.color = "#667eea";
+        inp.style.borderColor = "#667eea";
+    }
 
 });
