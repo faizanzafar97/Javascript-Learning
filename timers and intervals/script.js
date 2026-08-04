26969717d20ab900a7e29028971986dc42ef57c1
faizanzafar97 <faizanzafar8672@gmail.com>
2026-08-04 15:58:41 +0500

    timers and intervals practice

---
+++ timers and intervals/script.js
@@ -0,0 +1,11 @@
+let count = 10;
+
+let interval=setInterval(() => {
+    if (count >=0) {
+        console.log(count);
+        count--;
+    }
+    else{
+        clearInterval(interval);
+    }
+}, 1000);
