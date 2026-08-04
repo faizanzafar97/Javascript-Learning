42038d0ee75dad00980622f2bef24fa32b32d001
faizanzafar97 <faizanzafar8672@gmail.com>
2026-08-04 16:19:06 +0500

    download simulation practice

---
+++ timers and intervals/download_simulaton.js
@@ -0,0 +1,18 @@
+let count=0;
+
+let progress=document.querySelector("#progress-fill");
+let percentage=document.querySelector("#percentage");
+let progressstats=document.querySelector(".progress-stats");
+
+let st=setInterval(()=>{
+    if(count<=100){
+        progress.style.width=`${count}%`;
+        percentage.textContent=`${count}%`;
+        count++;
+    }
+
+    else{
+        progressstats.innerHTML="<span>Download Complete</span>";
+        clearInterval(st);
+    }    
+},1000/10);
