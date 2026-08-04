7e6b62d4aca55cb987877899b7d25fcd6b21f86b
faizanzafar97 <faizanzafar8672@gmail.com>
2026-08-04 16:27:22 +0500

    auto hide progrees bar after some seconds

--- timers and intervals/download_simulaton.js
+++ timers and intervals/download_simulaton.js
@@ -1,18 +1,26 @@
-let count=0;
+// let count=0;
 
-let progress=document.querySelector("#progress-fill");
-let percentage=document.querySelector("#percentage");
-let progressstats=document.querySelector(".progress-stats");
+// let progress=document.querySelector("#progress-fill");
+// let percentage=document.querySelector("#percentage");
+// let progressstats=document.querySelector(".progress-stats");
+
+// let st=setInterval(()=>{
+//     if(count<=100){
+//         progress.style.width=`${count}%`;
+//         percentage.textContent=`${count}%`;
+//         count++;
+//     }
+
+//     else{
+//         progressstats.innerHTML="<span>Download Complete</span>";
+//         clearInterval(st);
+//     }    
+// },1000/10);
+
+
+let sec=10;
+let div=document.querySelector(".progress-wrapper");
 
 let st=setInterval(()=>{
-    if(count<=100){
-        progress.style.width=`${count}%`;
-        percentage.textContent=`${count}%`;
-        count++;
-    }
-
-    else{
-        progressstats.innerHTML="<span>Download Complete</span>";
-        clearInterval(st);
-    }    
-},1000/10);
+    div.style.display="none";
+},sec*1000/100);GPUCommandBuffer
