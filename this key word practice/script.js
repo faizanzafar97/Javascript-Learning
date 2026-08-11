75365417bfad6987feae2c17ebb4c66e957f4801
faizanzafar97 <faizanzafar8672@gmail.com>
2026-08-11 19:59:58 +0500

    practice of call,bind,apply on this

--- this key word practice/script.js
+++ this key word practice/script.js
@@ -1,5 +1,4 @@
-
-        // 1. Global Scope
+// 1. Global Scope
         console.log("--- Global Scope ---");
         console.log(this);
 
@@ -21,20 +20,61 @@
         };
         a.s(); 
 
+        // event handler
+       
+        document.querySelector("h2").addEventListener("click", function(){
+            console.log((this.style.color="red"));
+        });
 
-// event handler
-document.querySelector("h2").addEventListener("click",
-    function(){
-    console.log((this.style.color="red"))
-})
+        //class handler
+        class abx{
+            constructor(){
+                console.log("hi");
+                this.b=12;
+            }  
+        }
+        let val=new abx();
+
+        //call apply bind
+        //function ko call karta wqt aap 
+        // this ki value set karte hau
+
+        // call
+
+        let obj= {
+            name:"faizan",
+            age:21,
+        };
+
+        function abcd(){
+            console.log(this.age);
+        }
+
+        abcd.call(obj);
+
+        //apply
+    let ob= {
+            name:"faizan",
+            age:21,
+        };
+
+        function bcd(a,b,c){
+            console.log(this.age,a,b,c);
+
+        }
+
+        bcd.apply(ob,[1,2,3]);
 
 
-//class handler
+    //bind
+    
+       let bj= {
+            name:"faizan",
+            age:21,
+        };
 
-class abx{
-    constructor(){
-        console.log("hi");
-        this.b=12;
-    }  
-}
-let val=new abx();
+        function abd(){
+            console.log(this.age);
+        }
+
+        let a = abd.bind(bj,1,2,3  );
